fbead888d3b73c5c00eda983424189453380a304
Anastasia Kravchenko <a.kravchenko2@g.nsu.ru>
2023-04-01 16:28:16 +0700

    added multithreading

--- sockets/server/server_runner.py
+++ sockets/server/server_runner.py
@@ -1,17 +1,32 @@
+import concurrent.futures
 import logging
 from network_manager.network_manager import NetworkManager
 from image_storage.image_storage_interface import ImageStorageInterface
 from image_storage.HDF5_image_storage import HDF5ImageStorage
+from server.image_buffer.image_buffer import ImageBuffer
 
 
 class ServerRunner:
-
     network_manager: NetworkManager
     image_storage: ImageStorageInterface
+    image_buffer: ImageBuffer
+
+    def run_network_manager(self):
+        self.network_manager = NetworkManager(self.image_storage)
+
+    def run_image_buffer(self):
+        self.image_buffer = ImageBuffer(self.image_storage)
 
     def __init__(self):
         self.image_storage = HDF5ImageStorage()
-        self.network_manager = NetworkManager(self.image_storage)
+
+        pool = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+
+        pool.submit(self.run_image_buffer)
+        pool.submit(self.run_network_manager)
+
+        # wait for all tasks to complete
+        pool.shutdown(wait=True)
 
 
 if __name__ == '__main__':
