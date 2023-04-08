d66c3dc1f7dd8bfed7db7ff60ffe8ffdf028347d
Anastasia Kravchenko <a.kravchenko2@g.nsu.ru>
2023-04-08 13:19:44 +0700

    added constants

--- sockets/utils/constants.py
+++ sockets/utils/constants.py
@@ -6,6 +6,8 @@ LOOP_DURATION = 1  # hours
 # CAMERA_OUTPUT_PATH = "/Users/tjest/Desktop/vscode/python/duckietown/camera_output/"
 CAMERA_OUTPUT_PATH = "/Users/anastasiakravcenko/Desktop/camera_output/"
 BYTE_CHUNK = 4096
+IMAGE_WIDTH = 960
+IMAGE_HEIGHT = 576
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
