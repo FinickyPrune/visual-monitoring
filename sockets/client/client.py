91d4fd0b0ec25c4b082e98aafc424622f4a040fd
TJesT <alexeynospid@gmail.com>
2023-04-08 16:31:26 +0700

    merged

--- sockets/client/client.py
+++ sockets/client/client.py
@@ -1,13 +1,58 @@
+from math import ceil, floor
 import os
 import pickle
 import socket
 import logging
+from time import sleep
+from typing import Generator, Tuple
 
 from PIL import Image
 from datetime import datetime
 
+import numpy as np
+
 from models.image import ImageDto
 from utils.constants import HOST, PORT, CAMERA_OUTPUT_PATH, BYTE_CHUNK
+
+
+class RedSquareGenerator:
+    def __init__(
+        self,
+        image_size: Tuple[int, int] = (100, 100),
+        square_count: int = 17,
+        square_size: Tuple[int, int] = (11, 11),
+    ):
+        self.__image_size: int = image_size
+        self.__target_count: int = square_count
+        self.__h, self.__w = square_size
+
+    def generate(self) -> Generator[None, None, Image.Image]:
+        shift = self.__image_size[1] / self.__target_count
+        RED = np.array((255, 0, 0), dtype=np.uint8)
+
+        for i in range(self.__target_count):
+            sleep(0.2)
+
+            im = Image.new("RGB", self.__image_size, "black")
+            pixels = im.load()
+            i_shifted = i * shift
+            current_x, next_x = floor(i_shifted), ceil(i_shifted)
+            first_density = i_shifted - current_x
+            last_density = next_x - i_shifted
+
+            for y in range(self.__h):
+                pixels[y, int(current_x)] = tuple(
+                    np.array(RED * first_density, dtype=np.uint8)
+                )
+                for x in range(1, self.__w - 1):
+                    pixels[
+                        y, min(int(current_x + x), self.__image_size[0] - 1)
+                    ] = tuple(RED)
+                pixels[
+                    y, min(int(current_x + self.__w - 1), self.__image_size[0] - 1)
+                ] = tuple(np.array(RED * last_density, dtype=np.uint8))
+
+            yield im
 
 
 class Client:
@@ -46,6 +91,6 @@ class Client:
         self.socket.send(dto_length_bytes)
 
         for i in range(0, len(dto_dump), BYTE_CHUNK):
-            self.socket.send(dto_dump[i: i + BYTE_CHUNK])
+            self.socket.send(dto_dump[i : i + BYTE_CHUNK])
 
         logging.info(f"{path} sent.")
--- sockets/server/image_buffer/image_buffer.py
+++ sockets/server/image_buffer/image_buffer.py
@@ -6,18 +6,25 @@ from datetime import timedelta
 from models.image import ImageDto
 from server.image_storage.image_storage_interface import ImageStorageInterface
 from server.sync_manager.sync_manager import SyncManager
-from utils.constants import UPDATE_STORAGE_INTERVAL, LOOP_DURATION, TIMESTAMP_FORMAT, DELIMITER
+from utils.constants import (
+    UPDATE_STORAGE_INTERVAL,
+    LOOP_DURATION,
+    TIMESTAMP_FORMAT,
+    DELIMITER,
+)
 
 
 class ImageBuffer:
     image_storage: ImageStorageInterface
-    image_dtos: [ImageDto] = []
+    image_dtos: list[ImageDto] = []
 
     def __init__(self, image_storage):
         self.image_storage = image_storage
         images, file_names = self.image_storage.load_all()
         for i in range(len(images)):
-            timestamp: datetime = datetime.strptime(file_names[i].split(DELIMITER)[2], TIMESTAMP_FORMAT)
+            timestamp: datetime = datetime.strptime(
+                file_names[i].split(DELIMITER)[2], TIMESTAMP_FORMAT
+            )
             self.image_dtos.append(ImageDto(timestamp, images[i]))
         SyncManager.sort_images(self.image_dtos)
 
@@ -34,8 +41,9 @@ class ImageBuffer:
             images, file_names = self.image_storage.load_all_after(timestamp)
 
         for i in range(len(images)):
-            timestamp: datetime = datetime.strptime(file_names[i].split(DELIMITER)[2], TIMESTAMP_FORMAT)
+            timestamp: datetime = datetime.strptime(
+                file_names[i].split(DELIMITER)[2], TIMESTAMP_FORMAT
+            )
             self.image_dtos.append(ImageDto(timestamp, images[i]))
         SyncManager.sort_images(self.image_dtos)
         logging.info(images)
-
